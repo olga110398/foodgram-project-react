23ac02ea5474456f806938d369d152c3eede868e
Ольга Аверкиева <olgaaverkieva1998@yandex.ru>
2023-10-02 16:00:55 +0300

    написана модель ингридиентов

---
+++ backend/recipes/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+class Ingredients(models.Model):
+    name = models.CharField(max_length=100)
+    units = models.CharField(max_length=50)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
